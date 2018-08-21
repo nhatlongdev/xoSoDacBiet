1bc0ef535881e9a5b092f47519a0b875eac368fa
nhatlongdev <nhatlongdev0986472126@gmail.com>
2018-08-21 22:30:40 +0700

    do so tt

--- components/ItemFLatListDoSo.js
+++ components/ItemFLatListDoSo.js
@@ -20,15 +20,15 @@ export default class ItemFlatListDoSo extends Component {
         return (
             <View>
                 <View style={{paddingHorizontal:5}}>
-                <Text style ={{color: Color.blue}}>{this.setTitleResultTraCuu(this.props.soTraCuu, this.props.data.length)}</Text> 
-                <View style = {{backgroundColor: 'red', flexDirection: 'row'}}>
+                <Text style ={{color: Color.blue}}>{this.setTitleResultTraCuu(this.props.item.so, this.props.item.arr_kq.length)}</Text> 
+                <View style = {{backgroundColor: 'grey', flexDirection: 'row'}}>
                     <Text style= {{flex: 2, textAlign: 'center', padding: 5, fontWeight: 'bold', color: 'white'}}>Số</Text>
                     <Text style= {{flex: 2, textAlign: 'center', padding: 5, fontWeight: 'bold', color: 'white'}}>Giải</Text>
                     <Text style= {{flex: 3, textAlign: 'center', padding: 5, fontWeight: 'bold', color: 'white'}}>Ngày</Text>
                 </View>
                 </View>
                 <FlatList   style={{paddingHorizontal:5}}
-                            data = {this.props.data}
+                            data = {this.props.item.arr_kq}
                             renderItem = {({item, index})=>{
                                 return(
                                     <ItemNumDetector
--- screens/NumberDetectorScreen.js
+++ screens/NumberDetectorScreen.js
@@ -27,14 +27,16 @@ var widthScreen = Dimensions.get('window').width;
 var heightScreen = Dimensions.get('window').height;
 var item_;
 var data_detector = [];
-var objResultDoSo = {};
+var objResultDoSo = [];
 var dataResultLottery = {};
 var soDo = '';
 var soLanQuay = 30;
+var arrSoDo;
 export default class NumberDetectorScreen extends Component {
     
     constructor(props){
         super(props);
+        arrSoDo = [];
         data_detector = [];
         this.state= {
             selected: data[0],
@@ -107,7 +109,7 @@ export default class NumberDetectorScreen extends Component {
                     />
                     <Text style={{fontSize: 15, fontWeight: 'bold', color: 'black'}}>Số dò (ví dụ: 66 hoặc 68,86):</Text>
                     <TextInput
-                        maxLength = {2} 
+                        maxLength = {5} 
                         placeholder={'Nhập số cần dò'}
                         placeholderTextColor = {'grey'}
                         onChangeText = {(text)=>this.setState({textSoDo: text})}
@@ -115,8 +117,8 @@ export default class NumberDetectorScreen extends Component {
                         value = {this.state.textSoDo}
                     />
                     <TouchableOpacity style={{flexDirection: 'row', alignItems: 'center',borderRadius: 2, backgroundColor: '#CCCCCC', height: 50,padding: 5}}
-                                    onPress = {()=>this.state.textSoDo.length == 2?this.numberDetector(item_, this.state.textSoDo, this.state.textSoLanQuay) : 
-                                        this.state.textSoDo.length == 0? alert('Vui lòng nhập số cần dò') : alert('Số dò phải gồm 2 số!')}
+                                    onPress = {()=>this.state.textSoDo.length !== 0?this.numberDetector(item_, this.state.textSoDo, this.state.textSoLanQuay) : 
+                                        alert('Vui lòng nhập số cần dò')}
                     >
                         <Text style={{flex: 1, textAlign: 'center', color: 'black', fontWeight: 'bold'}}>TRA CỨU LÔ TÔ, DÒ SỐ</Text>   
                         <Image
@@ -125,8 +127,61 @@ export default class NumberDetectorScreen extends Component {
                     </TouchableOpacity>
                     </View>
 
-                    <ItemFlatListDoSo soTraCuu={this.state.soDoTraCuu} data={data_detector}/>
+                    <FlatList   
+                            data = {objResultDoSo}
+                            renderItem = {({item, index})=>{
+                                return(
+                                    <ItemFlatListDoSo
+                                        item = {item} index = {index}
+                                    />
+                                );
+                            }}
+                            keyExtractor={ (item, index) => index.toString() }> 
+                    </FlatList>
 
+                    {/* {
+                        arrSoDo.length>0?
+                        <ItemFlatListDoSo soTraCuu={arrSoDo[0]} data={objResultDoSo[arrSoDo[0]]}/>:null
+                    } 
+
+                    {
+                        arrSoDo.length>1?
+                        <ItemFlatListDoSo soTraCuu={arrSoDo[1]} data={objResultDoSo[arrSoDo[1]]}/>:null
+                    }                         
+                   
+                    {
+                        arrSoDo.length>2?
+                        <ItemFlatListDoSo soTraCuu={arrSoDo[2]} data={objResultDoSo[arrSoDo[2]]}/>:null
+                    }  
+
+                    {
+                        arrSoDo.length>3?
+                        <ItemFlatListDoSo soTraCuu={arrSoDo[3]} data={objResultDoSo[arrSoDo[3]]}/>:null
+                    }  
+
+                    {
+                        arrSoDo.length>4?
+                        <ItemFlatListDoSo soTraCuu={arrSoDo[4]} data={objResultDoSo[arrSoDo[4]]}/>:null
+                    }  
+
+                    {
+                        arrSoDo.length>5?
+                        <ItemFlatListDoSo soTraCuu={arrSoDo[5]} data={objResultDoSo[arrSoDo[5]]}/>:null
+                    }  
+
+                    {
+                        arrSoDo.length>6?
+                        <ItemFlatListDoSo soTraCuu={arrSoDo[6]} data={objResultDoSo[arrSoDo[6]]}/>:null
+                    }  
+
+                    {
+                        arrSoDo.length>7?
+                        <ItemFlatListDoSo soTraCuu={arrSoDo[7]} data={objResultDoSo[arrSoDo[7]]}/>:null
+                    } 
+                    {
+                        arrSoDo.length>8?
+                        <ItemFlatListDoSo soTraCuu={arrSoDo[8]} data={objResultDoSo[arrSoDo[8]]}/>:null
+                    }   */}
                 </ScrollView>
                 <FloatButtonCompomentExit
                      onButtonFloatPress={this.clickExit.bind(this)}
@@ -136,37 +191,63 @@ export default class NumberDetectorScreen extends Component {
         );
     }
 
+    //hàm for để sét list kết quả dò số
+    showListResultDetector(arr){
+        if(arr.length>0){
+            for(let i=0; i<arr.length; i++){
+
+            }
+        }
+    }
+
     clickExit(){
         this.props.navigation.goBack();
     }
 
     numberDetector(_item, soDo, soLanQuay){
+
        //Tách chuỗi nhập vào ra thành mảng number
-       var arrSoDo = [];
+       arrSoDo = [];
        var arrSoDoTam  = [];
        var arrNumber = soDo.split(','); 
+        console.log("SO DO: " + arrNumber.length)
        for(let i =0;i<arrNumber.length;i++){
             if(arrNumber[i].length !== 2) break;
             if(dataBong[arrNumber[i]] != null){
+                console.log("CHAY VAO DK khac null ")
                 arrSoDoTam = arrSoDoTam.concat(dataBong[arrNumber[i]]);
             }
        } 
        
        //Loại bỏ phần tử trùng nhau trong mảng
-       if(arrSoDo.length >0){
+       if(arrSoDoTam.length >0){
             // set min count db, loto
             var _ = require('underscore');	
-            arrSoDo = _.uniq(arrSoDo);
+            arrSoDo = _.uniq(arrSoDoTam);
+            // alert(arrSoDo.length)
        }
-
 
         this.setState({
             soDoTraCuu: soDo +"",
         })
+
+        //Lấy mảng kết quả của tỉnh được chọn
         var arrLotteryOfProvinces = {};
         arrLotteryOfProvinces = dataResultLottery[_item.code];
         console.log('Data dua vao: ' + JSON.stringify(arrLotteryOfProvinces) + " -----" + arrLotteryOfProvinces.length)
 
+
+        for(let n=0; n<arrSoDo.length; n++){
+            let arr_kq = filterArrDetector(arrLotteryOfProvinces,arrSoDo[n],soLanQuay);
+            let obj = {};
+            obj.so = arrSoDo[n];
+            obj.arr_kq = arr_kq;
+            objResultDoSo.push(obj);
+        }
+
+        console.log('KET QUA: ' + JSON.stringify(objResultDoSo))
+
+        // chỗ này ko để làm gì nhưng tạm để lại để render lại giao diện
         data_detector = filterArrDetector(arrLotteryOfProvinces,soDo,soLanQuay);
         this.setState({
             data_detector: data_detector
